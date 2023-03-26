c5dea771151d1bea1bb6b02021a204465fd2d66f
msasif2000 <msasifiiuc@gmail.com>
2023-03-27 00:11:53 +0600

    2nd try

--- booklet/src/Components/Navbar/Navbar.js
+++ booklet/src/Components/Navbar/Navbar.js
@@ -1,35 +1,22 @@
 import Logo from "../../imgs/booklet-logo.png"
 import "./Navbar.css"
 import {Link} from "react-router-dom"
+import Search from '../Search/Search'
+import Categories from '../Categories/Categories'
+
 function Navbar(){
     return(
       <header className="Header">
       <Link to="/"><img src={Logo} alt="Booklet Logo" style={{ background:'rgb(28, 228, 228)', borderRadius:'7px', width:'auto', height:'6vh'}}/></Link>
 
-      
       <div className="navbar">
-			<Link to="/">what is Booklet?</Link>
+			<Link to="/">What is Booklet?</Link>
 			<Link to="/MyBook">My Book</Link>
-      <Link to="/Categories">Categories</Link>
+      {/* <Link to="/Categories">Categories</Link> */}
+      <Categories/>
 			<Link to="/Contact">Contact</Link>
-      </div>
-      <div className="searchbox">
-         <input id="searchbar" onkeyup="search_Books()" type="text"
-        name="search" placeholder="Search Books.."></input>
-        <ol id='list'>
-        <li class="books">Children's</li>
-        <li class="books">Computer</li>
-        <li class="books">Literary Fiction</li>
-        <li class="books">Magazines</li>
-        <li class="books">Career</li>
-        <li class="books">Poetry</li>
-        <li class="books">Lifestyle</li>
-        <li class="books">News</li>
-        <li class="books">Travel</li>
-        <li class="books">Religion</li>
-        <li class="books">Science and Technology</li>
-    </ol>
-    <script src="./books.js"></script>
+
+      <Search/>
       </div>
       </header>
     );
@@ -41,3 +28,5 @@ const Header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     Header.classList.toggle ("sticky", window.scrollY > null);
 });
+
+
