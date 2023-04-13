d55a719023866ae9f6f0ef1da2741a3541681ef2
msasif2000 <msasifiiuc@gmail.com>
2023-04-14 01:47:02 +0600

    Banner Added

--- booklet/src/Components/Pages/Home.js
+++ booklet/src/Components/Pages/Home.js
@@ -1,12 +1,26 @@
+// import React from 'react'
+// // import Offer from '../Offer/Offer'
+// import Careers from '../Careers/Career'
+
+// export default function Home() {
+//   return (
+//     <div className='Home'>
+      
+//       {/* <Offer/> */}
+//       <Careers/>
+//     </div>
+//   )
+// }
 import React from 'react'
-import Offer from '../Offer/Offer'
-import Careers from '../Careers/Career'
+// import Careers from '../Careers/Career'
+import "./Home.css"
 
 export default function Home() {
   return (
     <div className='Home'>
-      <Offer/>
-      <Careers/>
+      <div className='Main'> 
+        sd
+      </div>
     </div>
   )
 }
