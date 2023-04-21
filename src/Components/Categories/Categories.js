f5e98f06a763680d17e1ab5c938ddb4522c52397
Mohammad Shahin <c201035shahin.iiuc@gmail.com>
2023-04-22 01:19:38 +0600

    updated

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -5,17 +5,19 @@ import { Link } from 'react-router-dom';
 
 function DropdownItemTagsExample() {
   return (
-    <DropdownButton id="btn" title="Categories" variant="" >
-      <Dropdown.Item as="button"><Link to="/Childrens">Childrens</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/LiteraryFiction">Literary Fiction</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/ScienceTechnology">Science And Technology</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/CareerGrowth">Career And Growth</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/ReligionSpirituality">Religion And Spirituality</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/Magazines">Magazines</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/LanguageStudies">Language Studies</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/History">History</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/PoliticsPhilosophy">Politics And Philosophy</Link></Dropdown.Item>
-      <Dropdown.Item as="button"><Link to="/Travel">Travel</Link></Dropdown.Item>
+    <DropdownButton id="btn" title="Categories" variant="">
+      <div className="ip">
+      <Dropdown.Item as="button"><Link to="/Childrens" style={{color:'black'}}>Childrens</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/LiteraryFiction" style={{color:'black'}}>Literary Fiction</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/ScienceTechnology" style={{color:'black'}}>Science And Technology</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/CareerGrowth" style={{color:'black'}}>Career And Growth</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/ReligionSpirituality" style={{color:'black'}}>Religion And Spirituality</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/Magazines" style={{color:'black'}}>Magazines</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/LanguageStudies" style={{color:'black'}}>Language Studies</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/History" style={{color:'black'}}>History</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/PoliticsPhilosophy" style={{color:'black'}}>Politics And Philosophy</Link></Dropdown.Item>
+      <Dropdown.Item as="button"><Link to="/Travel" style={{color:'black'}}>Travel</Link></Dropdown.Item>
+      </div>
     </DropdownButton>
   );
 }
--- src/Components/Categories/Childrens.js
+++ src/Components/Categories/Childrens.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Childrens() {
   return (
     <div>
-      hi
+      
     </div>
   )
 }
