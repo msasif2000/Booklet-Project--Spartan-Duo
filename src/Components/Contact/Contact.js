a34f0ea5e9efb65c3f6f1f3b4eb2462588b9fcd6
Mohammad Shahin <c201035shahin.iiuc@gmail.com>
2023-06-15 23:54:27 +0600

    final

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,21 +7,77 @@ import { FaCcMastercard } from 'react-icons/fa';
 import { FaCcVisa } from 'react-icons/fa';
 import { FaPaypal } from 'react-icons/fa';
 import { FaCreditCard } from 'react-icons/fa';
-import React from 'react'
+import React, { useRef } from 'react'
+import { Button, Form } from 'react-bootstrap'
 import "./Contact.css"
 function Contact(){
+    const unameRef = useRef();
+    const mailRef = useRef();
+    const messageRef = useRef();
+
+    function handleSend(e){
+        e.preventDefault();
+        const uname = unameRef.current.value;
+        const mail = mailRef.current.value;
+        const message = messageRef.current.value;
+    
+        const feedback = { uname, mail, message };
+        
+        fetch('http://localhost:5000/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(feedback),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.insertedId) {
+                    alert('Data is Successfully added in database')
+                }
+                else {
+                    alert(data.status)
+                }
+            })
+
+        e.preventDefault();
+    }
     return(
         <div className="contact" id="contact">	
             <div className="contact-form">
                 <h2 className="text-center">Feedback</h2>
                 <p>Subscribe to our page to get the latest updates on our Book collection and posts.</p>
-                <form>
-                    <input type="" placeholder="Your Name" required/>
-                    <input type="email" name="" id="" placeholder="Your E-mail" required/>
-                    <input type="" placeholder="Write a subject" required/>
-                    <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
-                    <input type="submit" name="" value="Submit" className="send"/>
-                </form>
+                <Form style={{marginTop:"8vh", alignItems:'center'}}onSubmit={handleSend}>
+            <h1>User Registration</h1>
+          <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
+            <Form.Control className='MsgBox'
+              ref={unameRef}
+              type="text"
+              required
+              placeholder="Mr X"
+              style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
+            />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control className='MsgBox'
+            ref={mailRef}
+            type="email"
+            required
+            placeholder="example@hotmail.com"
+            style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control className='MsgBox'
+            ref={messageRef}
+            type="text"
+            required
+            placeholder="Enter Your Message"
+            style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">Send</Button>
+          </Form>
             </div>
     
             <div className="social">
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,45 +1,60 @@
-import Logo from "../../imgs/booklet-logo.png"
-import menu from "../../imgs/menu.png"
-import "./Navbar.css"
-import {Link} from "react-router-dom"
-import Categories from '../Categories/Categories'
-import { useState } from "react"
+import React, { useState, useEffect } from 'react';
+import Logo from "../../imgs/booklet-logo.png";
+import menu from "../../imgs/menu.png";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Categories from '../Categories/Categories';
 
-
-function Navbar(){
-  const isUser = localStorage.getItem('isUser')
+function Navbar() {
+  const isUser = localStorage.getItem('isUser');
   const handleSignOut = () => {
-    localStorage.removeItem('isUser')
-    window.location.href = '/'
-  }
-  const [toggle, setToggle] = useState(false)
-  const toggleMenu = () => {
-    setToggle(!toggle)
-  }
-  
-    return(
-      <header className="Header">
-      <Link to="/" style={{textDecoration:'none'}}><img src={Logo} alt="Booklet Logo" style={{ background:'bisque', borderRadius:'7px', width:'auto', height:'6vh'}}/></Link>
+    localStorage.removeItem('isUser');
+    window.location.href = '/';
+  };
+  const [toggle, setToggle] = useState(false);
 
-      <div className={toggle?"navbar active": "navbar"}>
-      <Link to="/" style={{textDecoration:'none'}} onClick={toggleMenu}>Home</Link>
-			<Link to="/AboutUs" style={{textDecoration:'none'}} onClick={toggleMenu}>About us</Link>
-		
-      {/* <Link to="/Categories">Categories</Link> */}
-      <Categories />
-			<Link to="/Contact" style={{textDecoration:'none'}} onClick={toggleMenu}>Contact</Link>
-      {
-        isUser ? <Link to="/Signin" style={{textDecoration:'none'}}onClick={handleSignOut}>Sign Out</Link> :
-        (
-          <div>
-          <Link to="/Signin" style={{textDecoration:'none'}} onClick={toggleMenu}>Sign In</Link>
-          <Link to="/Signup" style={{textDecoration:'none'}} onClick={toggleMenu}>Sign Up</Link>
-          </div>
-        )
-      }
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY > null);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <header className="Header">
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <img src={Logo} alt="Booklet Logo" style={{ background: 'bisque', borderRadius: '7px', width: 'auto', height: '6vh' }} />
+      </Link>
+
+      <div className={toggle ? "navbar active" : "navbar"}>
+        <Link to="/" style={{ textDecoration: 'none' }} onClick={toggleMenu}>Home</Link>
+        <Link to="/AboutUs" style={{ textDecoration: 'none' }} onClick={toggleMenu}>About us</Link>
+        {/* <Link to="/Categories">Categories</Link> */}
+        <Categories />
+        <Link to="/Contact" style={{ textDecoration: 'none' }} onClick={toggleMenu}>Contact</Link>
+        {
+          isUser ? <Link to="/Signin" style={{ textDecoration: 'none' }} onClick={handleSignOut}>Sign Out</Link> :
+            (
+              <div>
+                <Link to="/Signin" style={{ textDecoration: 'none' }} onClick={toggleMenu}>Sign In</Link>
+                <Link to="/Signup" style={{ textDecoration: 'none' }} onClick={toggleMenu}>Sign Up</Link>
+              </div>
+            )
+        }
       </div>
-      <button className="toggle"><img src={menu} alt="menu" style={{ background:'trandparent', borderRadius:'7px', width:'auto', height:'6vh'}} onClick={toggleMenu}/></button>
-      </header>
-    );
+      <button className="toggle"><img src={menu} alt="menu" style={{ background: 'trandparent', borderRadius: '7px', width: 'auto', height: '6vh' }} onClick={toggleMenu} /></button>
+    </header>
+  );
 }
+
 export default Navbar;
